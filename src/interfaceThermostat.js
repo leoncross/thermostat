a4b7815e2522712cf26f7ef4a5d617715015a5b6
leoncross <leon@lscross.co.uk>
2018-11-29 15:23:40 +0000

    up, down, reset buttons function as expected, with temperature updating as expected

--- src/interfaceThermostat.js
+++ src/interfaceThermostat.js
@@ -1,9 +1,27 @@
 $ ( document ).ready(function() {
 
+  var thermostat = new Thermostat
+  temperatureUpdate();
+
+$('#up').on('click', function() {
+  thermostat.up();
+  temperatureUpdate();
+})
+
+$('#down').on('click', function() {
+  thermostat.down();
+  temperatureUpdate();
+})
 
 
+$('#reset').on('click', function() {
+  thermostat.reset();
+  temperatureUpdate();
+})
 
-
+function temperatureUpdate() {
+  $('#temperature').text(thermostat.temperature);
+}
 
 
 });
